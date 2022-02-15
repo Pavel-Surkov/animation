44fddde4bb35704ba48a3720245f11b20f5ddb0d
Tarun <tarun@triterras.com>
2022-02-15 23:56:22 +0530

    Home Page mobile view updated

--- src/components/Public/Homepage/Email/Email.js
+++ src/components/Public/Homepage/Email/Email.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Space, Input, Button } from 'antd'
 import classes from './Email.module.scss'
-const Email = () => (
-  <div className={classes.container}>
-    <h2>
-      Signup to get firsthand information <br /> on what’s coming soon!
-    </h2>
-    <div className={classes.emailSection}>
-      <Space>
-        <Input placeholder="Email address..." size="large" />
-        <Button type="primary" size="large">
-          Submit
-        </Button>
-      </Space>
+import { MailOutlined } from '@ant-design/icons'
+const Email = () => {
+  const [value, setValue] = useState('')
+
+  const [screenSize, getDimension] = useState({
+    dynamicWidth: window.innerWidth,
+    dynamicHeight: window.innerHeight,
+  })
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+      dynamicHeight: window.innerHeight,
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', setDimension)
+
+    return () => {
+      window.removeEventListener('resize', setDimension)
+    }
+  }, [screenSize])
+
+  return (
+    <div className={classes.container}>
+      <h2>
+        Signup to get firsthand information <br /> on what’s coming soon!
+      </h2>
+      <div className={classes.emailSection}>
+        <Space size={screenSize.dynamicWidth < 500 ? 0 : 'large'}>
+          <Input placeholder="Email address..." size="large" />
+          <Button type="primary" size="large">
+            {screenSize.dynamicWidth < 500 ? <MailOutlined /> : 'Submit'}
+          </Button>
+        </Space>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 export default Email
