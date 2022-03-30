0623b3b34ede78bd7a764fb1c76474d9f0adb2cf
Tarun <tarun@triterras.com>
2022-03-30 11:01:30 +0530

    saga implementaion

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import userReducer from '../../src/redux/reducers/userReducer'
 import CounterReducer from '../CounterSlice'
+
 export default configureStore({
   reducer: {
     counter: CounterReducer,
+    user: userReducer,
   },
 })
