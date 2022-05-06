3ff256951cd692d46554b74846c9f4ba573feb11
Pavel Surkov <surkovv.pavel@gmail.com>
2022-05-06 13:45:02 +0700

    adds desktop version of categories section

--- src/components/Public/Homepage/TrendingCategories/TrendingCategories.js
+++ src/components/Public/Homepage/TrendingCategories/TrendingCategories.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './categories.scss';
 import HomeCategoryItem from '../HomeCategoryItem/HomeCategoryItem';
+import ButtonWithRightArrow from '../../../../constant/public/ButtonWithRightArrow/ButtonWithRightArrow';
 import clothes from '../../../../assets/images/clothing-line-1.jpg';
 import clothes2x from '../../../../assets/images/clothing-line-1@2x.jpg';
+import cosmetics from '../../../../assets/images/cosmetics-1.jpg';
+import cosmetics2x from '../../../../assets/images/cosmetics-1@2x.jpg';
+import jewelry from '../../../../assets/images/jewelry-1.jpg';
+import jewelry2x from '../../../../assets/images/jewelry-1@2x.jpg';
+import furniture from '../../../../assets/images/furniture-1.jpg';
+import furniture2x from '../../../../assets/images/furniture-1@2x.jpg';
+import textile from '../../../../assets/images/textile-1.jpg';
+import textile2x from '../../../../assets/images/textile-1@2x.jpg';
 
 const TrendingCategories = () => {
   return (
@@ -16,6 +25,35 @@ const TrendingCategories = () => {
             <HomeCategoryItem
               title={'[APPAREL]'}
               image={{ quality1x: clothes, quality2x: clothes2x }}
+            />
+            <HomeCategoryItem
+              title={'[COSMETICS]'}
+              image={{ quality1x: cosmetics, quality2x: cosmetics2x }}
+            />
+            <HomeCategoryItem
+              title={'[JEWELRY]'}
+              image={{ quality1x: jewelry, quality2x: jewelry2x }}
+            />
+            <div className="category-item category-item_text">
+              <h3 className="title title_item category-item_text__title">
+                Can’t find the category you are looking for?
+              </h3>
+              <div className="category-item__subtitle">
+                <p>
+                  Contact us and we will find you suppliers within our network.
+                </p>
+              </div>
+              <div className="category-item__see">
+                <ButtonWithRightArrow content={'See more'} />
+              </div>
+            </div>
+            <HomeCategoryItem
+              title={'[FURNITURE]'}
+              image={{ quality1x: furniture, quality2x: furniture2x }}
+            />
+            <HomeCategoryItem
+              title={'[TEXTILE]'}
+              image={{ quality1x: textile, quality2x: textile2x }}
             />
           </div>
         </div>
--- src/constant/public/ButtonWithRightArrow/ButtonWithRightArrow.js
+++ src/constant/public/ButtonWithRightArrow/ButtonWithRightArrow.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import classes from './ButtonWithRightArrow.module.scss'
-import { Row, Col } from 'antd'
-import { Space } from 'antd'
+import React from 'react';
+import classes from './ButtonWithRightArrow.module.scss';
+import { Row, Col } from 'antd';
+import { Space } from 'antd';
 
-import arrowRight from '../../../assets/svg/Arrow_Right.svg'
+import arrowRight from '../../../assets/svg/Arrow_Right.svg';
 
 const ButtonWithRightArrow = (props) => {
   return (
     <>
       <button
         disabled={props.disabled === undefined ? false : props.disabled}
-        className={props.disabled ? classes.buttonDisabled : classes.button}
+        className={
+          props.disabled ? classes.buttonDisabled : 'button button-arrow'
+        }
         onClick={props.function}
       >
-        <Row>
-          <Col span={18}>{props.content}</Col>
-          <Col span={6} align="right">
-            <img src={arrowRight} alt="Uplio" />
-          </Col>
-        </Row>
+        <p>{props.content}</p>
+        <img width="55" src={arrowRight} alt="Uplio" />
       </button>
     </>
-  )
-}
-export default ButtonWithRightArrow
+  );
+};
+export default ButtonWithRightArrow;
