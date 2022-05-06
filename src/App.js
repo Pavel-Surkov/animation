fa97fcb2d53c511c74fccc8e3212235983217e2d
Pavel Surkov <surkovv.pavel@gmail.com>
2022-05-06 12:51:46 +0700

    changes scss import to make style cascading correct

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MainLayout from './layouts/MainLayout/MainLayout';
 import { BrowserRouter } from 'react-router-dom';
 import './App.less';
-import './App.scss';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
