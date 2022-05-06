ac523f38288e7e18ca5cef6b524d65bfaf114627
Pavel Surkov <surkovv.pavel@gmail.com>
2022-05-06 18:53:42 +0700

    adds mobile version of footer

--- src/constant/public/Footer/Footer.js
+++ src/constant/public/Footer/Footer.js
@@ -57,6 +57,38 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
+            <ul className="footer-socials footer-socials_m">
+              <li className="footer-socials__item">
+                <a
+                  className="link footer-socials__link"
+                  target="_blank"
+                  href="https://www.instagram.com/uplio_marketplace/"
+                  rel="noreferrer"
+                >
+                  Instagram
+                </a>
+              </li>
+              <li className="footer-socials__item">
+                <a
+                  className="link footer-socials__link"
+                  target="_blank"
+                  href="https://twitter.com/Uplio11"
+                  rel="noreferrer"
+                >
+                  Twitter
+                </a>
+              </li>
+              <li className="footer-socials__item">
+                <a
+                  className="link footer-socials__link"
+                  target="_blank"
+                  href="https://www.facebook.com/Uplio-103062572308513"
+                  rel="noreferrer"
+                >
+                  Facebook
+                </a>
+              </li>
+            </ul>
             <div className="footer-categories">
               <p className="footer-categories__title">Categories</p>
               <ul className="footer-categories__list">
@@ -112,7 +144,7 @@ const Footer = () => {
               </li>
             </ul>
             <div className="footer-contact">
-              <p className="footer-contact__title">Contact</p>
+              <p className="footer-contact__title">Contact:</p>
               <div className="footer-contact__contact">
                 <a href="mailto:hello@uplio.com">hello@uplio.com</a>
               </div>
@@ -142,6 +174,18 @@ const Footer = () => {
                     className="button footer-subscribtion__submit"
                   >
                     Subscribe
+                    <svg
+                      width="56"
+                      height="16"
+                      viewBox="0 0 56 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M55.7071 8.70711C56.0976 8.31658 56.0976 7.68342 55.7071 7.29289L49.3431 0.928932C48.9526 0.538408 48.3195 0.538408 47.9289 0.928932C47.5384 1.31946 47.5384 1.95262 47.9289 2.34315L53.5858 8L47.9289 13.6569C47.5384 14.0474 47.5384 14.6805 47.9289 15.0711C48.3195 15.4616 48.9526 15.4616 49.3431 15.0711L55.7071 8.70711ZM0 9H55V7H0V9Z"
+                        fill="white"
+                      />
+                    </svg>
                   </button>
                 </div>
               </form>
