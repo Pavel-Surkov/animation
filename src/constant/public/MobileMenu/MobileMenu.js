73b818b480b525007887235a8ffb2d83480f5b56
Pavel Surkov <surkovv.pavel@gmail.com>
2022-05-10 12:58:16 +0700

    adds navigation layout for login state

--- src/constant/public/MobileMenu/MobileMenu.js
+++ src/constant/public/MobileMenu/MobileMenu.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './m-menu.scss';
 
 export const MobileMenu = ({ isSigned, menuOpened, setMenuOpened }) => {
+  const user = useSelector((state) => state.user.user);
+
   return (
     <div className={`m-menu ${menuOpened ? 'm-menu_opened' : null}`}>
       <div className="container">
@@ -10,7 +13,11 @@ export const MobileMenu = ({ isSigned, menuOpened, setMenuOpened }) => {
           <div className="m-menu__search">
             <input className="input" type="text" placeholder="Search" />
           </div>
-          <ul className="m-menu-links">
+          <ul
+            className={`m-menu-links ${
+              isSigned ? 'm-menu-links_signed' : null
+            }`}
+          >
             <li className="m-menu-links__link">
               <a
                 className="link"
@@ -49,7 +56,66 @@ export const MobileMenu = ({ isSigned, menuOpened, setMenuOpened }) => {
               </Link>
             </div>
           )}
-          {isSigned && <div className="header__account"></div>}
+          {isSigned && (
+            <div className="m-menu-signed">
+              <h4 className="title title_item-s m-menu-signed__title">
+                {user ? `Hi ${user.name}!` : `Hi John!`}
+              </h4>
+              <ul className="m-menu-signed__list">
+                <li className="m-menu-signed__item">
+                  <Link
+                    className="title title_item-s m-menu-signed__link"
+                    to=""
+                  >
+                    Orders
+                  </Link>
+                </li>
+                <li className="m-menu-signed__item m-menu-signed__item_messages">
+                  <Link
+                    className="title title_item-s m-menu-signed__link"
+                    to=""
+                  >
+                    Message Center
+                  </Link>
+                  <div className="m-menu__messages">
+                    {user ? user.messages : '2'}
+                  </div>
+                </li>
+                <li className="m-menu-signed__item">
+                  <Link
+                    className="title title_item-s m-menu-signed__link"
+                    to="/quote/generic_quotation"
+                  >
+                    Quotes
+                  </Link>
+                </li>
+                <li className="m-menu-signed__item">
+                  <Link
+                    className="title title_item-s m-menu-signed__link"
+                    to=""
+                  >
+                    Bookmarks
+                  </Link>
+                </li>
+                <li className="m-menu-signed__item">
+                  <Link
+                    className="title title_item-s m-menu-signed__link"
+                    to=""
+                  >
+                    Account Settings
+                  </Link>
+                </li>
+                <li className="m-menu-signed__item">
+                  <Link
+                    className="title title_item-s m-menu-signed__link"
+                    to=""
+                  >
+                    Log Out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
